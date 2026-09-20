7f994c8aee7462ffbcb515828990fbdf69e39163
huangbo <huangbo@jiancent.com>
2026-09-20 09:26:02 +0800

    chore: 根目录清理（删一次性脚本/文档归docs/删空文件）

--- fix-home-mount.py
+++
@@ -1,8 +0,0 @@
-import io
-p = r"C:\Users\jiancent\WorkBuddy\fund\fund-simulator\frontend\src\views\Home.vue"
-with io.open(p, 'r', encoding='utf-8') as f:
-    s = f.read()
-s = s.replace("onMounted(() => {\n  loadHomeData()\n  startLiveStream()\n})", "onMounted(() => {\n  loadAllData()\n  startLiveStream()\n})")
-with io.open(p, 'w', encoding='utf-8', newline='\n') as f:
-    f.write(s)
-print('fixed')
